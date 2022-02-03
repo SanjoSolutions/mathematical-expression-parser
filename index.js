a23be8dfa575bbf2637478e1a718dd68de12be20
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-02-03 21:46:03 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,118 @@
+const EXPRESSION_REGEXP = /^\(?([^\(]+) ([\+]) (.+)\)?$/
+
+const NUMBER_REGEXP_STRING = '\\d+(?:\\.\\d+)?'
+
+const OPERAND_REGEXP = new RegExp(NUMBER_REGEXP_STRING)
+
+const NUMBER_FROM_START_REGEXP = new RegExp(`^${NUMBER_REGEXP_STRING}`)
+
+const PLUS_STRING = ' + '
+
+export const Operator = {
+  PLUS: '+',
+}
+
+export function add(a, b) {
+  return a + b
+}
+
+export function parse(expression) {
+  return new Tree(parseSubexpression((expression)))
+}
+
+function parseSubexpression(expression) {
+  const match = EXPRESSION_REGEXP.exec(expression)
+  if (match) {
+    const [_, firstOperand, operator, secondOperand] = match
+    return new OperationNode(parseSubexpression(firstOperand), parseOperator(operator), parseSubexpression(secondOperand))
+  } else {
+    try {
+      return parseOperand(expression)
+    } catch (error) {
+      throw new Error(`Invalid expression: "${ expression }"`)
+    }
+  }
+}
+
+function parseOperand(operand) {
+  const match = OPERAND_REGEXP.exec(operand)
+  if (match) {
+    const value = Number(match[0])
+    const node = new OperandNode(value)
+    return node
+  } else {
+    throw new Error('Invalid operand')
+  }
+}
+
+function parseOperator(operator) {
+  if (operator === '+') {
+    return Operator.PLUS
+  } else {
+    throw new Error('Unknown operator')
+  }
+}
+
+function extractNumberFromTheStart(string) {
+  const numberMatch = NUMBER_FROM_START_REGEXP.exec(string)
+  if (numberMatch) {
+    return numberMatch[0]
+  } else {
+    throw new Error(`Unexpected expression: "${ string }"`)
+  }
+}
+
+export function extractNextGroupedSubexpression(expression) {
+  const a = expression.indexOf('(')
+  if (a !== -1) {
+    let openGroups = 0
+    let fromIndex = a + 1
+    do {
+      const b = expression.indexOf('(', fromIndex)
+      const c = expression.indexOf(')', fromIndex)
+      if (b === -1 || c < b) {
+        if (openGroups === 0) {
+          return {
+            fromIndex: a,
+            toIndex: c + 1,
+            value: expression.slice(a + 1, c),
+          }
+        } else if (c !== -1) {
+          openGroups--
+          if (openGroups < 0) {
+            throw new Error('Invalid expression: more closing parenthesis than opening parenthesis.')
+          }
+        }
+      } else {
+        openGroups++
+      }
+      fromIndex = b !== -1 ? b + 1 : c + 1
+    } while (true)
+  }
+}
+
+export class Tree {
+  constructor(root) {
+    this.root = root
+  }
+}
+
+export class Node {
+
+}
+
+export class OperandNode extends Node {
+  constructor(value) {
+    super()
+    this.value = value
+  }
+}
+
+export class OperationNode extends Node {
+  constructor(firstOperand, operator, secondOperand) {
+    super()
+    this.firstOperand = firstOperand
+    this.operator = operator
+    this.secondOperand = secondOperand
+  }
+}
---
+++ index.spec.js
@@ -0,0 +1,50 @@
+import { parse, Tree, Operator, extractNextGroupedSubexpression, OperationNode, OperandNode } from './index.js'
+
+describe('parse', () => {
+  it('parses "1 + 2"', () => {
+    const tree = parse('1 + 2')
+    expect(tree).toEqual(
+      new Tree(new OperationNode(new OperandNode(1), Operator.PLUS, new OperandNode(2)))
+    )
+  })
+
+  it('parses "1 + 2 + 3"', () => {
+    const tree = parse('1 + 2 + 3')
+    expect(tree).toEqual(
+      new Tree(new OperationNode(
+        new OperationNode(new OperandNode(1), Operator.PLUS, new OperandNode(2)),
+        Operator.PLUS,
+        new OperandNode(3)
+      ))
+    )
+  })
+
+  it('parses "1 + (2 + 3)"', () => {
+    const tree = parse('1 + (2 + 3)')
+    expect(tree).toEqual(
+      new Tree(new OperationNode(
+        new OperandNode(1),
+        Operator.PLUS,
+        new OperationNode(new OperandNode(2), Operator.PLUS, new OperandNode(3))
+      ))
+    )
+  })
+})
+
+describe('extract next grouped subexpression', () => {
+  it('works with one group', () => {
+    expect(extractNextGroupedSubexpression('1 + (2 + 3)')).toEqual({
+      fromIndex: 4,
+      toIndex: 11,
+      value: '2 + 3'
+    })
+  })
+
+  it('works with nested groups', () => {
+    expect(extractNextGroupedSubexpression('((1 + 2) + 3)')).toEqual({
+      fromIndex: 0,
+      toIndex: 13,
+      value: '(1 + 2) + 3'
+    })
+  })
+})
